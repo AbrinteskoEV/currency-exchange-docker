b9fc359d7d23c926d7478ea35ac54d4abc979bb7
Evgeniy Abrintesko <eabrintesko@dev-gns.com>
2023-09-05 14:12:06 +0300

    Released cron into php docker

--- docker/currency-exchange/php-fpm/entrypoint.cli.sh
+++ docker/currency-exchange/php-fpm/entrypoint.cli.sh
@@ -6,5 +6,6 @@ ssh-keyscan github.com >> ~/.ssh/known_hosts
 git clone git@github.com:AbrinteskoEV/currency-exchange.git/ .
 
 composer install
-php-fpm --nodaemonize
+php-fpm -D
+sudo crond -f -l 8
 sleep infinity
