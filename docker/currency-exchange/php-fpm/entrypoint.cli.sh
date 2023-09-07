8e01ce3edeb2000f2e6067e5181c14bd5c6762f8
Evgeniy Abrintesko <eabrintesko@dev-gns.com>
2023-09-08 00:31:49 +0300

    rework git connection

--- docker/currency-exchange/php-fpm/entrypoint.cli.sh
+++ docker/currency-exchange/php-fpm/entrypoint.cli.sh
@@ -3,7 +3,7 @@
 sudo chown oem:oem /var/www
 
 ssh-keyscan github.com >> ~/.ssh/known_hosts
-git clone git@github.com:AbrinteskoEV/currency-exchange.git/ .
+git clone https://github.com/AbrinteskoEV/currency-exchange.git/ .
 
 composer install
 php-fpm -D
--- docker/tarantool/entrypoint.cli.sh
+++ docker/tarantool/entrypoint.cli.sh
@@ -3,7 +3,7 @@
 su root --command "sudo chown tarantool:tarantool /usr/share/tarantool/currency-exchange"
 
 ssh-keyscan github.com >> ~/.ssh/known_hosts
-git clone git@github.com:AbrinteskoEV/currency-exchange-tarantool.git/ .
+git clone https://github.com/AbrinteskoEV/currency-exchange-tarantool.git/ .
 
 TARANTOOL_WORKDIR=/var/lib/tarantool/currency-exchange.${TARANTOOL_INSTANCE_NAME:-default}
 TARANTOOL_PID_FILE=/var/run/tarantool/currency-exchange.${TARANTOOL_INSTANCE_NAME:-default}.pid
